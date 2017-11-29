43c2858596b0a4925d738a918c9a5283f9498290
Ted Chen <tedchen@KKStreamde-MacBook-Pro.local>
2017-11-29 17:54:09 +0800

    Fix bug

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except ImportError:
 home_dir = os.path.expanduser('~')
 credential_dir = path_join(home_dir, '.credentials')
 client_secret = path_join(home_dir, 'client_secret.json')
-processed_events = "processed_events"
+processed_events_file = "processed_events"
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 APPLICATION_NAME = 'Google Calendar API Hook'
@@ -54,38 +54,44 @@ class CalenderAPI(object):
 
         result = self.service.events().list(
             # calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-            calendarId=calender_id, timeMin=now, singleEvents=True,
-            orderBy='startTime').execute()
+            calendarId=calender_id, timeMin=now, singleEvents=False, showDeleted=True).execute()
 
+        all_raw_events = result.get('items', [])
         events = []
-        for event in result.get('items', []):
+        for event in all_raw_events:
             events += [{
                 "id": event["id"],
-                "summary": event["summary"],
-                "start": event["start"]["dateTime"],
-                "end": event["end"]["dateTime"],
-                "attendees": [x["email"] for x in event["attendees"]]
+                "status": event["status"],
+                "summary": event["summary"] if "summary" in event else "",
+                "start": event["start"]["dateTime"] if "dateTime" in event["start"] else event["start"]["date"],
+                "end": event["end"]["dateTime"] if "dateTime" in event["end"] else event["end"]["date"],
+                "attendees": [x["email"] for x in event["attendees"]] if "attendees" in event else []
             }]
         return events
 
 
 if __name__ == '__main__':
-    pattern = "[Yo]"
+    pattern = "OTT Builder"
     now = datetime.datetime.utcnow().isoformat()
     api = CalenderAPI()
-    cur_events = [x for x in api.get_events("primary") if x["summary"].startswith(pattern)]
+    # cur_events = [x for x in api.get_events("kkbox.com_5c6vv9p0ee4dk70cb6rmm54kao@group.calendar.google.com") if x["summary"].startswith(pattern)]
+    cur_events = [x for x in api.get_events("kkbox.com_5c6vv9p0ee4dk70cb6rmm54kao@group.calendar.google.com")]
     msg_buf = ""
 
-    if os.path.isfile(processed_events):
-        with open(processed_events, "rb") as fin:
+    if os.path.isfile(processed_events_file):
+        with open(processed_events_file, "rb") as fin:
             processed_events = pickle.load(fin)
             # Filter old data
-            processed_events = [x for x in processed_events if now < x["end"]]
+            cancelled_events = [x for x in cur_events if x["status"] == "cancelled"]
+            non_cancelled_events = [x for x in cur_events if x["status"] != "cancelled"]
 
             processed_event_ids = [x["id"] for x in processed_events]
-            cur_event_ids = [x["id"] for x in cur_events]
-            del_event_ids = list(set(processed_event_ids) - set(cur_event_ids))
-            new_event_ids = list(set(cur_event_ids) - set(processed_event_ids))
+            non_cancelled_event_ids = [x["id"] for x in non_cancelled_events]
+            cancelled_event_ids = [x["id"] for x in cancelled_events]
+            del_event_ids = list(set(processed_event_ids) & set(cancelled_event_ids))
+            # print str(set(processed_event_ids))
+            # print str(set(cur_event_ids))
+            new_event_ids = list(set(non_cancelled_event_ids) - set(processed_event_ids))
             remained_processed_event_ids = list(set(processed_event_ids) - set(del_event_ids))
 
             store_events = [x for x in cur_events if x["id"] in remained_processed_event_ids]
@@ -96,16 +102,10 @@ if __name__ == '__main__':
             for event in [x for x in cur_events if x["id"] in new_event_ids]:
                 msg_buf += "New event '{}'\n".format(event["summary"])
                 # Do something
-                store_events += [event]
-            store_events = processed_events
+                store_events += [dict(event)]
     else:
-        store_events = cur_events
+        store_events = [x for x in cur_events if x["status"] != "cancelled"]
         msg_buf += "Initialize...\n"
-    with open(processed_events, "wb+") as fout:
+    with open(processed_events_file, "wb+") as fout:
         pickle.dump(store_events, fout)
     print msg_buf
-
-
-
-
-    print str(api.get_events("primary"))
